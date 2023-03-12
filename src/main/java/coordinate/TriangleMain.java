3f6b75c5d39cccbf422d056f6e1f6dfa98f27a7b
남의영 <ny2485@naver.com>
2023-03-12 19:27:57 +0900

    feat(young1) : 4.좌표계산기(삼각형면적)

--- src/main/java/coordinate/TriangleMain.java
+++ src/main/java/coordinate/TriangleMain.java
@@ -1,2 +1,17 @@
-package coordinate;public class TriangleMain {
+package coordinate;
+
+import coordinate.domain.Triangle;
+import coordinate.view.InputView;
+import coordinate.view.OutputView;
+
+public class TriangleMain {
+    public static void main(String[] args) {
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        outputView.initNotice();
+        Triangle triangle = inputView.triangleInit();
+        outputView.main(triangle.getPoints());
+        outputView.printTriangleResult(triangle);
+
+    }
 }
--- src/main/java/coordinate/domain/Triangle.java
+++ src/main/java/coordinate/domain/Triangle.java
@@ -1,2 +1,35 @@
-package coordinate.domain;public class Triangle {
+package coordinate.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Triangle {
+
+    ArrayList<Point> points;
+
+    public String getTriangleArea() {
+        Point pointA = points.get(0);
+        Point pointB = points.get(1);
+        Point pointC = points.get(2);
+
+        double heron = heron(initLine(pointA, pointB).getPointLength(), initLine(pointB, pointC).getPointLength(), initLine(pointC, pointA).getPointLength());
+        String strNum = String.format("%.1f", heron);
+        return strNum;
+    }
+
+    private static Line initLine(Point pointA, Point pointB) {
+        return new Line(new ArrayList<>(Arrays.asList(pointA, pointB)));
+    }
+
+    private double heron(double a, double b, double c) {
+        return 0.25 * Math.sqrt((a + b + c) * (b + c - a) * (a + c - b) * (a + b - c));
+    }
+    public Triangle(ArrayList<Point> points) {
+        this.points = points;
+    }
+
+    public ArrayList<Point> getPoints() {
+        return points;
+    }
+
 }
